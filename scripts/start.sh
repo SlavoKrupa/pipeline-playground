c30ff5e156104ae0815775ef6aaa4cadb130c083
SlavoKrupa <slavomir.krupa@gmail.com>
2016-09-20 13:14:23 +0200

    added my startup scripts and changed template

---
+++ scripts/start.sh
@@ -0,0 +1,58 @@
+# Provide path to your oc command which can work with 3.3 openshift (can do oc cluster up)
+oc='oc_rc'
+$oc version
+$oc cluster up
+# hotfix for a alpha3 which was broken
+#$oc cluster up --version=v1.3.0-alpha.2
+# create CICD namesapce and setup service accounts - then create CI/CD infrastructure
+$oc new-project cicd --display-name="CI/CD"
+$oc policy add-role-to-user edit system:serviceaccount:cicd:default -n cicd
+$oc process -f complete.yaml | oca create -f -
+
+# create dev and stage projects and switch back to cicd
+$oc new-project dev --display-name="Tasks - Dev" > /dev/null
+$oc new-project stage --display-name="Tasks - Stage" > /dev/null
+$oc policy add-role-to-user edit system:serviceaccount:cicd:default -n dev
+$oc policy add-role-to-user edit system:serviceaccount:cicd:default -n stage
+$oc project cicd
+echo "Waiting for a gogs to come online"
+x=1
+RETURN=$(curl -o /dev/null -sL -w "%{http_code}" http://gogs-cicd.10.40.3.141.xip.io/user/login)
+while [ ! $RETURN -eq 200 ]
+    do
+      sleep 5
+      printf %s .
+      x=$(( $x + 1 ))
+       if [ $x -gt 100 ]
+      then
+        echo ""
+        echo "Gogs did not start correctly."
+        exit 255
+      fi
+       RETURN=$(curl -o /dev/null -sL -w "%{http_code}" http://gogs-cicd.10.40.3.141.xip.io/user/login)
+    done
+echo ""
+echo "Gogs is online: http://gogs-cicd.10.40.3.141.xip.io/user/login"
+$oc get dc gogs -o yaml | grep GOGS -A 1
+echo "Waiting for a jenkins to come online"
+x=1
+RETURN=$(curl -o /dev/null -sL -w "%{http_code}" http://jenkins-cicd.10.40.3.141.xip.io/login)
+while [ ! $RETURN -eq 200 ]
+    do
+      sleep 5
+      printf %s .
+      x=$(( $x + 1 ))
+       if [ $x -gt 100 ]
+      then
+        echo ""
+        echo "Jenkins did not start correctly."
+        exit 255
+      fi
+       RETURN=$(curl -o /dev/null -sL -w "%{http_code}" http://jenkins-cicd.10.40.3.141.xip.io/login)
+    done
+echo ""
+echo "Jenkins is online: http://jenkins-cicd.10.40.3.141.xip.io/login"
+$oc get dc jenkins -o yaml | grep JENKINS -A 1
+
+
+
